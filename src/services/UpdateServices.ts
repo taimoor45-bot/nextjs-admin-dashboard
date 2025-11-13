c4b91f75614fb25d0ddba282299c04e27fec2202
Taimoor Hussain <taimoorhussain279@gmail.com>
2025-11-13 19:35:41 +0500

    fix Is approved

---
+++ src/services/UpdateServices.ts
@@ -0,0 +1,14 @@
+import { supabase } from "../../supabase/supabase-admin";
+
+export async function updateQuoteStatus(id: string, status: boolean) {
+  const { data, error } = await supabase
+    .from("quotes")
+    .update({ is_approved: status })
+    .eq("id", id);
+
+  if (error) {
+    console.log(error.message);
+    return false;
+  }
+  return true;
+}
